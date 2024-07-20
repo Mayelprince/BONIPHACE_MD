eb5491ea7d9f2ac140b48c6da53f65acd8a80a4d
boniphace704 <anthonyboniphace704@gmail.com>
2024-07-20 09:38:17 +0300

    Update GPT.js

--- commandes/GPT.js
+++ commandes/GPT.js
@@ -8,7 +8,7 @@ Created By fredie tech
 Don't claim, okey 
 */
 
-zokou({nomCom:"gpt4",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
+zokou({nomCom:"gpt4",reaction:"🅱️",categorie:"IA"},async(dest,zk,commandeOptions)=>{
 
   const {repondre,ms,arg}=commandeOptions;
   
