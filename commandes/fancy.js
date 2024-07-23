559536b9918bdb968a5bacd51422661c08806292
boniphace tech <anthonyboniphace704@gmail.com>
2024-07-23 15:53:28 +0300

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "🅱️" }, async (dest, z
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 fredie tech\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('BONIPHACE-MD 🅱️', fancy));
+            return await repondre(`\nExemple : ${prefixe} POWERED BY BONIPHACE TEACH\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('BONIPHACE-MD 🅱️', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
